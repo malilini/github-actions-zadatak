6edea28cac7790b547c5d03fc04765d4dce09b63
Sanja Krmpotic <sanja.krmpotic1@gmail.com>
2021-04-19 20:44:11 +0200

    Prvi commit

---
+++ about.php
@@ -0,0 +1,21 @@
+<?php
+
+echo "Stranica je u nastanku, molimo za strpljenje";
+echo "<br><br>";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>O nama</title>
+</head>
+<body>
+    <div>
+    <a href="index.php"><< Povratak na početnu</a>
+</div>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+echo "Dobrodošli na test-github-actions";
+echo "<br><br>"
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Naslovna</title>
+</head>
+    <body>
+        <div>
+            <a href="about.php">O nama</a>
+        </div>
+        <br><br>
+        <div>
+            <a href="privacy.php">Privatnost</a>
+        </div>
+    </body>
+</html>
